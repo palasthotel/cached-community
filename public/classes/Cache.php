b10117a9781f770c26a72ff5c75eb8a324ab9c8a
EdwardBock <edward.bock@palasthotel.de>
2021-01-18 16:42:12 +0100

    purge on comment

--- public/classes/Cache.php
+++ public/classes/Cache.php
@@ -2,15 +2,24 @@
 
 namespace CachedCommunity;
 
+use WP_Comment;
+
 class Cache extends _Component {
 
 	const GET_NO_CACHE = "no_cache";
+	private $purged;
 
 	function onCreate() {
+
+		$this->purged = [];
+
 		if( $_SERVER['REQUEST_METHOD'] === 'GET' ){
 			add_action('template_redirect', array($this, 'early_template_redirect'), 0);
 			add_action('template_redirect', array($this, 'late_template_redirect'), 99);
 		}
+
+		add_action('comment_post', [$this, 'comment_post'], 10, 2);
+		add_action('transition_comment_status', [$this, 'transition_comment_status'], 10, 3);
 	}
 
 	/**
@@ -39,6 +48,56 @@ class Cache extends _Component {
 				exit;
 			}
 		}
+	}
+
+	/**
+	 *
+	 * @param $comment_id
+	 * @param $status
+	 */
+	public function comment_post($comment_id, $status){
+		if($status == 1 || $status == "approved" || $status == "approve"){
+			$this->purgeCommentPost($comment_id);
+		}
+	}
+
+	/**
+	 * watch status transitions to notify all commentators
+	 * @param string $new_status
+	 * @param string $old_status
+	 * @param WP_Comment $comment
+	 */
+	public function transition_comment_status($new_status, $old_status, $comment){
+		$new_is_public = $new_status == 1 || $new_status == "approve" || $new_status == "approved";
+		$old_is_public = $new_status == 1 || $new_status == "approve" || $new_status == "approved";
+
+		if( $new_is_public != $old_is_public){
+			$this->purgeCommentPost($comment->comment_ID);
+		}
+
+	}
+
+	private function purgeCommentPost($comment_id){
+		$comment = get_comment($comment_id);
+		if( 'comment' != $comment->comment_type) return;
+
+		if( in_array($comment->comment_post_ID, $this->purged) ) return;
+		$this->purged[] = $comment->comment_post_ID;
+
+		$curlOptionList = array(
+			CURLOPT_RETURNTRANSFER    => true,
+			CURLOPT_CUSTOMREQUEST     => 'PURGE',
+			CURLOPT_HEADER            => true ,
+			CURLOPT_NOBODY            => true,
+			CURLOPT_URL               => get_permalink($comment->comment_post_ID),
+			CURLOPT_CONNECTTIMEOUT_MS => 3000
+		);
+
+		$curlHandler = curl_init();
+		curl_setopt_array( $curlHandler, $curlOptionList );
+		curl_exec($curlHandler);
+		error_log("PURGED ".get_the_title($comment->comment_post_ID));
+
 	}
 
 }
